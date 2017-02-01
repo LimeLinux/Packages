3bc95ca404f368135d8c78c5be73942204e52639
poyraz76 <ergunsalman@hotmail.com>
2017-02-01 15:20:06 +0300

    add

---
+++ desktop/toolkit/qtcreator/actions.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+# Licensed under the GNU General Public License, version 3.
+# See the file http://www.gnu.org/copyleft/gpl.txt
+
+from pisi.actionsapi import qt5
+from pisi.actionsapi import shelltools
+from pisi.actionsapi import pisitools
+from pisi.actionsapi import get
+
+WorkDir="qt-creator-opensource-src-%s" % get.srcVERSION()
+
+def setup():
+    shelltools.system("qmake qtcreator.pro CONFIG+=release QTC_PREFIX=/usr")
+
+
+def build():
+    qt5.make()
+
+def install():
+    qt5.install()
+    pisitools.rename("/usr/bin/qtcreator", "qtcreator-bin")
+    pisitools.domove("/usr/share/qtcreator/debugger/LICENSE.GPL3-EXCEPT", "/usr/share/licenses/qtcreator")
