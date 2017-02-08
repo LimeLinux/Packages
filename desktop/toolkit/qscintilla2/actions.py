04692b428c514c83095cc2445cc219cc3f92fdec
poyraz76 <ergunsalman@hotmail.com>
2017-02-08 22:58:34 +0300

    tuhaf temizlik

--- desktop/toolkit/qscintilla2/actions.py
+++ desktop/toolkit/qscintilla2/actions.py
@@ -18,58 +18,15 @@ def setup():
     shelltools.cd("Qt4Qt5")
     shelltools.system("qmake qscintilla.pro")
 
-    # Change C/XXFLAGS
-    pisitools.dosed("Makefile", "^CFLAGS.*\\$\\(DEFINES\\)", "CFLAGS   = %s -fPIC $(DEFINES)" % get.CFLAGS())
-    pisitools.dosed("Makefile", "^CXXFLAGS.*\\$\\(DEFINES\\)", "CXXFLAGS   = %s -fPIC $(DEFINES)" % get.CXXFLAGS())
-
-    # Get designer plugin's Makefile
-    shelltools.cd("../designer-Qt4Qt5/")
-    shelltools.system("qmake designer.pro INCLUDEPATH+=../Qt4Qt5 QMAKE_LIBDIR+=../Qt4Qt5")
-
-    # Change C/XXFLAGS of designer plugin's makefile
-    pisitools.dosed("Makefile", "^CFLAGS.*\\$\\(DEFINES\\)", "CFLAGS   = %s -fPIC $(DEFINES)" % get.CFLAGS())
-    pisitools.dosed("Makefile", "^CXXFLAGS.*\\$\\(DEFINES\\)", "CXXFLAGS   = %s -fPIC $(DEFINES)" % get.CXXFLAGS())
 
 def build():
-    shelltools.system("cp -rf Python Python3")
     shelltools.cd("Qt4Qt5")
-    qt5.make()
-
-    shelltools.cd("../designer-Qt4Qt5/")
-    qt5.make()
-
-    # Get Makefile of qscintilla-python via sip
-    shelltools.cd("../Python")
-    pythonmodules.run("configure.py -n ../Qt4Qt5 -o ../Qt4Qt5 -c --pyqt=PyQt5 --pyqt-sipdir=/usr/share/sip/Py2Qt5 --qsci-sipdir=/usr/share/sip/Py2Qt5 --sip-incdir=/usr/lib/python2.7/site-packages --qmake /usr/bin/qmake")
-    pisitools.dosed("Makefile", "/usr/include/qt/QtPrintSupport", "/usr/include/qt5/QtPrintSupport")
-    pisitools.dosed("Makefile", "/usr/include/qt/QtWidgets", "/usr/include/qt5/QtWidgets")
     autotools.make()
 
-    shelltools.cd("../Python3")
-    pythonmodules.run("configure.py -n ../Qt4Qt5 -o ../Qt4Qt5 -c --pyqt=PyQt5 --qmake /usr/bin/qmake", pyVer = "3")
-    pisitools.dosed("Makefile", "/usr/include/qt/QtPrintSupport", "/usr/include/qt5/QtPrintSupport")
-    pisitools.dosed("Makefile", "/usr/include/qt/QtWidgets", "/usr/include/qt5/QtWidgets")
-    autotools.make()
+
 
 def install():
     shelltools.cd("Qt4Qt5")
-    qt5.install("INSTALL_ROOT=%s" % get.installDIR())
+    autotools.install("INSTALL_ROOT=%s" % get.installDIR())
 
-    shelltools.cd("../designer-Qt4Qt5/")
-    qt5.install("INSTALL_ROOT=%s" % get.installDIR())
 
-    #build and install qscintilla-python
-    shelltools.cd("../Python3")
-    #autotools.rawInstall("DESTDIR=%s" % get.installDIR())
-    qt5.install("INSTALL_ROOT=%s" % get.installDIR())
-    pisitools.insinto("/usr/lib/python3.6/site-packages/PyQt5", "Qsci.so")
-    shelltools.cd("../Python")
-    #autotools.rawInstall("DESTDIR=%s" % get.installDIR())
-    qt5.install("INSTALL_ROOT=%s" % get.installDIR())
-    pisitools.insinto("/usr/lib/python2.7/site-packages/PyQt5", "Qsci.so")
-
-    shelltools.cd("..")
-    pisitools.dohtml("doc/html-Qt4Qt5/")
-    pisitools.insinto("/usr/share/doc/%s/Scintilla" % get.srcNAME(), "doc/Scintilla/*")
-
-    pisitools.dodoc("LICENSE*", "NEWS", "README")
