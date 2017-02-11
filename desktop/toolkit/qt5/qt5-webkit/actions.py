f0f223c74ff58aa20c9a761ba95c48c94a249079
poyraz76 <ergunsalman@hotmail.com>
2017-02-11 15:46:35 +0300

    update-2

--- desktop/toolkit/qt5/qt5-webkit/actions.py
+++ desktop/toolkit/qt5/qt5-webkit/actions.py
@@ -11,17 +11,17 @@ from pisi.actionsapi import qt5
 from pisi.actionsapi import get
 
 def setup():
-    qt5.configure(parameters="QMAKE_CFLAGS_ISYSTEM= ")
+    qt5.configure(parameters="QMAKE_CFLAGS_ISYSTEM=")
 
 def build():
     qt5.make()
     qt5.make("docs")
 
 def install():
-    qt5.install("INSTALL_ROOT=%s install_docs" % get.installDIR())
+    autotools.install("INSTALL_ROOT=%s install_docs" % get.installDIR())
 
     #I hope qtchooser will manage this issue
-    for bin in shelltools.ls("%s/usr/lib/qt5/bin" % get.installDIR()):
-        pisitools.dosym("/usr/lib/qt5/bin/%s" % bin, "/usr/bin/%s-qt5" % bin)
+    #for bin in shelltools.ls("%s/usr/lib/qt5/bin" % get.installDIR()):
+     #   pisitools.dosym("/usr/lib/qt5/bin/%s" % bin, "/usr/bin/%s-qt5" % bin)
 
     pisitools.dodoc("LICENSE.GPLv2", "LICENSE.LGPLv3", "LICENSE.LGPLv21", "ChangeLog-2012-05-22")
