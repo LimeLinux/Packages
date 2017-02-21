81180f0d7f71948fcef4f1f6f2a1233bdcabe1be
poyraz76 <ergunsalman@hotmail.com>
2017-02-21 20:34:51 +0300

    update

--- system/devel/gcc/actions.py
+++ system/devel/gcc/actions.py
@@ -1,30 +1,35 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 #
-# Licensed under the GNU General Public License, version 2.
-# See the file http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt
+# Licensed under the GNU General Public License, version 3.
+# See the file http://www.gnu.org/licenses/gpl.txt
 
-from pisi.actionsapi import shelltools
 from pisi.actionsapi import autotools
 from pisi.actionsapi import pisitools
+from pisi.actionsapi import shelltools
 from pisi.actionsapi import get
 
 import os
 
-verMajor = get.srcVERSION()
+snapshot = False
+
+if snapshot:
+    verMajor, verMinor = get.srcVERSION().replace("pre", "").split("_", 1)
+    WorkDir = "gcc-4.7-%s" % verMinor
+else:
+    verMajor = get.srcVERSION()
+
 arch = get.ARCH().replace("x86_64", "x86-64")
+
+opt_arch = "--with-arch=armv7-a --with-float=hard --with-fpu=vfpv3-d16" if get.ARCH() == "armv7h" else "--with-arch_32=i686"
+
+opt_multilib = "--enable-multilib" if get.ARCH() == "x86_64" else "--disable-multilib"
 
 # WARNING: even -fomit-frame-pointer may break the build, stack protector, fortify source etc. are off limits
 cflags = "-O2 -g"
 
-def removeLimeSection(_dir):
-    for root, dirs, files in os.walk(_dir):
-        for name in files:
-            # FIXME: should we do this only on nonshared or all ?
-            # if ("crt" in name and name.endswith(".o")) or name.endswith("nonshared.a"):
-            if ("crt" in name and name.endswith(".o")) or name.endswith(".a"):
-                i = os.path.join(root, name)
-                shelltools.system('objcopy -R ".comment.LIMELINUX.OPTs" -R ".note.gnu.build-id" %s' % i)
+
+
 
 def exportFlags():
     # we set real flags with new configure settings, these are just safe optimizations
@@ -35,31 +40,26 @@ def exportFlags():
     # FIXME: this may not be necessary for biarch
     shelltools.export("CC", "gcc")
     shelltools.export("CXX", "g++")
-    shelltools.export("LC_ALL", "en_US.UTF-8")
+
 
 def setup():
     exportFlags()
+    pisitools.flags.remove("-pipe")
     # Maintainer mode off, do not force recreation of generated files
-    # shelltools.system("contrib/gcc_update --touch")
+    #shelltools.system("contrib/gcc_update --touch")
     pisitools.dosed("gcc/Makefile.in", "\.\/fixinc\.sh", "-c true")
     pisitools.dosed("gcc/configure", "^(ac_cpp='\$CPP\s\$CPPFLAGS)", r"\1 -O2")
     pisitools.dosed("libiberty/configure", "^(ac_cpp='\$CPP\s\$CPPFLAGS)", r"\1 -O2")
     
     pisitools.dosed("gcc/config/i386/t-linux64", "^(ac_cpp='\$CPP\s\$CPPFLAGS)", r"\1 -O2")
     pisitools.system("sed -i '/m64=/s/lib64/lib/' gcc/config/i386/t-linux64")
+   
 
-    # Do not install libiberty
-    # This one comes with binutils
-    pisitools.dosed("libiberty/Makefile.in", "install_to_\$\(INSTALL_DEST\) ", "")
-
-    # Do not run fixincludes
-    pisitools.dosed("gcc/Makefile.in", "\./fixinc\.sh", "-c true")
-
+    shelltools.cd("../")
     shelltools.makedirs("build")
     shelltools.cd("build")
 
-    # multilib is disabled
-    shelltools.system('../configure \
+    shelltools.system('.././gcc-%s/configure \
                        --prefix=/usr \
                        --bindir=/usr/bin \
                        --libdir=/usr/lib \
@@ -67,42 +67,41 @@ def setup():
                        --includedir=/usr/include \
                        --mandir=/usr/share/man \
                        --infodir=/usr/share/info \
-                       --build=%s \
-                       --with-gxx-include-dir=/usr/include/c++ \
-                       --enable-linker-build-id \
-                       --enable-ssp \
-                       --enable-clocale=gnu \
-                       --enable-libstdcxx-allocator=new \
+                       --with-bugurl=http://bugs.limelinux.com \
+                       --enable-languages=c,c++,fortran,lto,objc,obj-c++ \
+                       --disable-libgcj \
                        --enable-shared \
                        --enable-threads=posix \
-                       --enable-checking=release \
-                       --enable-multilib \
-                       --enable-bootstrap \
                        --with-system-zlib \
-                       --with-system-libunwind \
                        --enable-__cxa_atexit \
-                       --enable-gnu-unique-object \
-                       --enable-languages=c,c++,fortran,objc,obj-c++,lto \
-                       --enable-plugin \
-                       --disable-nls \
-                       --disable-libgcj \
                        --disable-libunwind-exceptions \
+                       --enable-clocale=gnu \
                        --disable-libstdcxx-pch \
-                       --without-included-gettext \
-                       --with-arch_32=i686 \
-                       --with-tune=generic \
-                       --with-pkgversion="Lime Linux" \
-                       --with-bugurl=http://bugs.limelinux.com' % get.HOST())
+                       --disable-libssp \
+                       --enable-gnu-unique-object \
+                       --enable-linker-build-id \
+                       --enable-cloog-backend=isl \
+                       --disable-cloog-version-check \
+                       --enable-lto \
+                       --enable-plugin \
+                       --with-linker-hash-style=gnu \
+                       --with-pkgversion="Pisi Linux" \
+                       --disable-werror \
+                       --enable-checking=release \
+                       --build=%s \
+                               %s \
+                               %s ' % ( verMajor , get.HOST(), opt_arch, opt_multilib))
 
 def build():
     exportFlags()
 
-    shelltools.cd("build")
-    autotools.make('BOOT_CFLAGS="%s" profiledbootstrap' % cflags)
+    shelltools.cd("../build")
+    autotools.make()
 
 def install():
-    shelltools.cd("build")
+    shelltools.cd("../build")
     autotools.rawInstall("DESTDIR=%s" % get.installDIR())
+    #autotools.install()
 
     for header in ["limits.h","syslimits.h"]:
         pisitools.insinto("/usr/lib/gcc/%s/%s/include" % (get.HOST(), verMajor) , "gcc/include-fixed/%s" % header)
@@ -111,18 +110,15 @@ def install():
     pisitools.removeDir("/usr/lib/gcc/*/*/include-fixed")
     pisitools.removeDir("/usr/lib/gcc/*/*/install-tools")
 
+    # This one comes with binutils
+    #pisitools.remove("/usr/lib*/libiberty.a")
+
     # cc symlink
     pisitools.dosym("/usr/bin/gcc" , "/usr/bin/cc")
 
     # /lib/cpp symlink for legacy X11 stuff
     pisitools.dosym("/usr/bin/cpp", "/lib/cpp")
 
-    # Remove our options section from crt stuff
-    """
-    removeLimeSection("%s/usr/lib/" % get.installDIR())
-    if get.ARCH() == "x86_64":
-        removeLimeSection("%s/usr/lib32/" % get.installDIR())
-    """
 
     # autoload gdb pretty printers
     gdbpy_dir = "/usr/share/gdb/auto-load/usr/lib/"
@@ -131,11 +127,5 @@ def install():
     gdbpy_files = shelltools.ls("%s/usr/lib/libstdc++*gdb.py*" % get.installDIR())
     for i in gdbpy_files:
         pisitools.domove("/usr/lib/%s" % shelltools.baseName(i), gdbpy_dir)
-
-
-
-
-
-
 
 
