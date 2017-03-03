30c6cf9777004be81f2082ffd555e379f48bc6ac
poyraz76 <ergunsalman@hotmail.com>
2017-03-03 22:16:34 +0000

    add+update

---
+++ multimedia/video/gst-plugins-good/actions.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+#
+# Licensed under the GNU General Public License, version 3.
+# See the file http://www.gnu.org/licenses/gpl.txt
+
+from pisi.actionsapi import autotools
+from pisi.actionsapi import pisitools
+
+def setup():
+    pisitools.dosed("configure.ac", "AM_CONFIG_HEADER", "AC_CONFIG_HEADERS")
+    autotools.configure("--with-package-name='LimeLinux gstreamer-plugins-good package' \
+                         --with-package-origin='http://limelinux.com' \
+                         --with-gtk=3.0")
+
+    pisitools.dosed("libtool", " -shared ", " -Wl,-O1,--as-needed -shared ")
+
+def build():
+    autotools.make()
+
+def install():
+    autotools.install()
+
+    pisitools.dodoc("AUTHORS", "README", "RELEASE")
---
+++ programming/language/python/python-imaging/actions.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+#
+# Licensed under the GNU General Public License, version 3.
+# See the file http://www.gnu.org/licenses/gpl.txt
+
+from pisi.actionsapi import pisitools
+from pisi.actionsapi import pythonmodules
+from pisi.actionsapi import shelltools
+from pisi.actionsapi import get
+
+WorkDir="Imaging-%s" % get.srcVERSION()
+
+def install():
+    pythonmodules.install()
+
+    shelltools.cd("Sane")
+    pythonmodules.install()
+    shelltools.cd("..")
+
+    for header in ["Imaging.h","ImPlatform.h"]:
+        pisitools.insinto("/usr/include/%s" % get.curPYTHON(), "libImaging/%s" % header)
+
+    pisitools.dodoc("README")
+
