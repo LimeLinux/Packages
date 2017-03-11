0fd08ff3e2f7f5c6723921ffdef837bdec27949e
poyraz76 <ergunsalman@hotmail.com>
2017-03-11 16:23:12 +0000

    update

--- programming/language/python/python-sphinx/actions.py
+++ programming/language/python/python-sphinx/actions.py
@@ -8,7 +8,7 @@ from pisi.actionsapi import pythonmodules
 from pisi.actionsapi import pisitools
 from pisi.actionsapi import get
 
-#WorkDir = "Sphinx-%s" % get.srcVERSION()
+WorkDir = "Sphinx-%s" % get.srcVERSION()
 
 def build():
     pythonmodules.compile()
@@ -20,9 +20,6 @@ def install():
     pythonmodules.run("-c \"from sphinx.pycode.pgen2.driver import load_grammar ; \
 load_grammar('%s/usr/lib/%s/site-packages/sphinx/pycode/Grammar-py2.txt')\"" %(get.installDIR(), get.curPYTHON(), ) )
 
-    # create sphinx documentation using itself
-    pythonmodules.run("sphinx-build.py doc doc/_build/html")
-    pisitools.dohtml("doc/_build/html/*")
 
-    pisitools.dodoc("CHANGES", "EXAMPLES")
+    pisitools.dodoc("CHANGES", "EXAMPLES", "AUTHORS", "LICENSE", "README.rst")
 
