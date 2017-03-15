08a82e7a14eabca4aa274dd0135681f9a78ba407
poyraz76 <ergunsalman@hotmail.com>
2017-03-15 15:05:55 +0000

    update

--- programming/language/python/python-mutagen/actions.py
+++ programming/language/python/python-mutagen/actions.py
@@ -1,16 +1,13 @@
 #!/usr/bin/python
-# -*- coding: utf-8 -*-
-#
-# Licensed under the GNU General Public License, version 3.
-# See the file http://www.gnu.org/licenses/gpl.txt
 
 from pisi.actionsapi import pythonmodules
-from pisi.actionsapi import pisitools
-from pisi.actionsapi import get
 
-WorkDir = "mutagen-%s" % get.srcVERSION()
+
+def build():
+    pythonmodules.compile()
+    pythonmodules.compile(pyVer = "3")
+
 
 def install():
     pythonmodules.install()
-
-    pisitools.dodoc("NEWS", "COPYING", "README.rst")
+    pythonmodules.install(pyVer = "3")
