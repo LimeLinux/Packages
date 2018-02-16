0db87b470ee8956fc2a5cdcffba0028844702132
poyraz76 <ergunsalman@hotmail.com>
2018-02-16 03:54:35 +0300

    baselayout version bump

--- system/base/openrc/comar/package.py
+++ system/base/openrc/comar/package.py
@@ -4,3 +4,10 @@ import os
 
 def postInstall(fromVersion, fromRelease, toVersion, toRelease):
     os.popen("/sbin/telinit u &> /dev/null")
+    os.system("rc-update add agetty.tty1 default")
+    os.system("rc-update add agetty.tty2 default")
+    os.system("rc-update add agetty.tty3 default")
+    os.system("rc-update add agetty.tty4 default")
+    os.system("rc-update add agetty.tty5 default")
+    os.system("rc-update add agetty.tty6 default")
+
