834a1cec7e1978e8f872fbf35545060c032bbecf
poyraz76 <ergunsalman@hotmail.com>
2018-02-27 23:36:19 +0300

    update

--- multimedia/stream/rtmpdump/actions.py
+++ multimedia/stream/rtmpdump/actions.py
@@ -5,12 +5,15 @@
 # See the file http://www.gnu.org/copyleft/gpl.txt.
 
 from pisi.actionsapi import autotools
-from pisi.actionsapi import pisitools
+from pisi.actionsapi import pisitools , shelltools
 from pisi.actionsapi import get
 
 makeargs="prefix=/usr sbindir=/usr/bin mandir=/usr/share/man"
 
 def setup():
+    shelltools.system("git clone https://git.ffmpeg.org/rtmpdump.git")
+    shelltools.cd("rtmpdump") 
+    shelltools.system("sed -e 's/^CRYPTO=OPENSSL/#CRYPTO=OPENSSL/' -e 's/#CRYPTO=GNUTLS/CRYPTO=GNUTLS/' -i Makefile -i librtmp/Makefile")
     pisitools.dosed("Makefile", "CC=.*", "CC=%s" % get.CC())
     pisitools.dosed("Makefile", "LDFLAGS=.*", \
                                 "LDFLAGS=%s" % get.LDFLAGS())
@@ -20,10 +23,11 @@ def setup():
 
 
 def build():
-        
+    shelltools.cd("rtmpdump")  
     autotools.make(' -j1 %s XCFLAGS="%s"' % (makeargs, get.CFLAGS()) )
 
 def install():
+    shelltools.cd("rtmpdump")
     autotools.rawInstall(' -j1 %s DESTDIR=%s' % (makeargs, get.installDIR()))
 
     #pisitools.remove("/usr/lib/librtmp.a")
