8b11dbae0ca361a412915cd1b6d1f8b341ae9196
poyraz76 <ergunsalman@hotmail.com>
2018-03-02 20:36:46 +0300

    update

--- programming/misc/libconfig/actions.py
+++ programming/misc/libconfig/actions.py
@@ -9,6 +9,7 @@ from pisi.actionsapi import pisitools
 from pisi.actionsapi import get
 
 def setup():
+    autotools.autoreconf("-vi")
     autotools.configure("--disable-static")
 
     # Do not link to unused m library
