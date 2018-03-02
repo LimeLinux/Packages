262de8e08f5b437136918465b4c7067c1d44d68a
poyraz76 <ergunsalman@hotmail.com>
2018-03-02 21:13:13 +0300

    update

--- programming/library/yaml-cpp/actions.py
+++ programming/library/yaml-cpp/actions.py
@@ -5,14 +5,15 @@
 # See the file http://www.gnu.org/licenses/gpl.txt
 
 from pisi.actionsapi import pisitools
-from pisi.actionsapi import cmaketools
+from pisi.actionsapi import cmaketools, get
 
 def setup():
-    cmaketools.configure()
+    cmaketools.configure("-DBUILD_SHARED_LIBS=ON \
+                          -DCMAKE_BUILD_TYPE=Release")
     
 def build():
     cmaketools.make()
 
 def install():
-    cmaketools.install()
+    cmaketools.rawInstall("DESTDIR=%s" % get.installDIR())
 
