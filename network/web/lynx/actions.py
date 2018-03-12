26caeaf7ed5d270e87b502e1326d675df48e6af3
poyraz76 <ergunsalman@hotmail.com>
2018-03-12 20:01:57 +0300

    update

--- network/web/lynx/actions.py
+++ network/web/lynx/actions.py
@@ -8,21 +8,7 @@ from pisi.actionsapi import get
 from pisi.actionsapi import pisitools
 
 def setup():
-    autotools.configure("--prefix=/usr \
-                         --sysconfdir=/etc \
-                         --enable-warnings \
-                         --enable-8bit-toupper \
-                         --enable-externs \
-                         --enable-cgi-links \
-                         --enable-persistent-cookies \
-                         --enable-prettysrc \
-                         --enable-source-cache \
-                         --enable-charset-choice \
-                         --enable-default-colors \
-                         --enable-nested-tables \
-                         --enable-read-eta \
-                         --with-zlib \
-                         --enable-nls \
+    autotools.configure("--enable-nls \
                          --enable-ipv6 \
                          --mandir=/usr/share/man")
 
